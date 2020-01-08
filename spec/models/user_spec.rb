b6dcf4a1eb9a75db5483f7235f1b1b28ff70e051
Gerardo Rocha <daniel0416@gmail.com>
2020-01-08 01:05:27 -0600

    Add User model and its test

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let!(:role) { Role.create }
+
+  context 'associations' do
+    subject { User.new(role: role)}
+    it { is_expected.to validate_uniqueness_of(:email) }
+    it { is_expected.to have_secure_password }
+    it { is_expected.to belong_to(:role) }
+  end
+end
