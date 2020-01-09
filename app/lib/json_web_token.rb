de7d806cc9aebc86791f4210177bb2d7725d89a8
Gerardo Rocha <daniel0416@gmail.com>
2020-01-09 17:55:59 -0600

    Use of JWT to encode and create a auth token

---
+++ app/lib/json_web_token.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class JsonWebToken
+  SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
+
+  def self.encode(payload, exp = 24.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, SECRET_KEY)
+  end
+end
