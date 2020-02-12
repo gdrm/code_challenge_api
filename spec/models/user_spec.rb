b8787b4e948ce7ca9e9be4eea44da073e34e1e7e
alx-api <ialexk@gmail.com>
2020-02-12 17:35:28 -0600

    specs for has_many associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,5 +10,6 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_uniqueness_of(:email) }
     it { is_expected.to have_secure_password }
     it { is_expected.to belong_to(:role) }
+    it { should have_many(:comments)}
   end
 end
