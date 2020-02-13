22f47a5d1cc5aa3d9dc4b0e33c54ad7d66565ab3
alx-api <ialexk@gmail.com>
2020-02-12 19:05:46 -0600

    using default subject in comment spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
 
   context 'associations' do
-    (Comment.new)
     it { should belong_to(:user) }
     it { should belong_to(:solution) }
   end
